c63ec7b01174767944baf7b5d8833beddb031a45
Yusuf <mdyusuf2521@gmail.com>
2024-02-07 18:11:52 +0530

    cors origin updated2

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ connect((db) => {
     const tokenRoutes = require('./routes/tokenRoutes');
 
     app.use(cors({
-        origin: ['http://localhost:3000','https://online-chess-lz0k.onrender.com/'],
+        origin:'*',
     }))
 
     // app.use(cookieParser);
